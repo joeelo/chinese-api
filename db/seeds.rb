ec4742313c440d5d81d425bc823c8ead75725024
Joseph <joeephus@gmail.com>
2019-03-06 18:01:03 -0500

    pretty functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ Like.destroy_all
 
 
 characters = [
+  
   {pronunciation: "shi", chinese: "是", meaning: "to be"},
   {pronunciation: "wo", chinese: "我", meaning: "I"},
   {pronunciation: "bu", chinese: "不", meaning: "not / negation"},
@@ -34,13 +35,37 @@ characters = [
   {pronunciation: "gōng", chinese: "工", meaning: "work"},
   {pronunciation: "kāi", chinese: "开", meaning: "open"},
   {pronunciation: "xīn", chinese: "心", meaning: "heart"},
-  {pronunciation: "kāixīn", chinese: "开心", meaning: "happy"},
+  {pronunciation: "de", chinese: "的", meaning: "posessive particle"},
+  {pronunciation: "le", chinese: "了", meaning: "completed action"},
+  {pronunciation: "tā", chinese: "他", meaning: "he / him"},
+  {pronunciation: "gè", chinese: "个", meaning: "general measure word"},
+  {pronunciation: "yǒu", chinese: "有", meaning: "to have; there is"},
+  {pronunciation: "lái", chinese: "来", meaning: "to come"},
+  {pronunciation: "nà", chinese: "那", meaning: "that"},
+  {pronunciation: "yào", chinese: "要", meaning: "to want; important"},
+  {pronunciation: "huì", chinese: "会", meaning: "can; will; to know"},
+  {pronunciation: "shén", chinese: "什", meaning: "what"},
+  {pronunciation: "méi", chinese: "没", meaning: "not; negation"},
+  {pronunciation: "dào", chinese: "到", meaning: "to arrive; until (time)"},
+  {pronunciation: "shuō", chinese: "说", meaning: "to speak"},
+  {pronunciation: "néng", chinese: "能", meaning: "to be able to"},
+  {pronunciation: "shàng", chinese: "上", meaning: "above; top"},
+  {pronunciation: "qù", chinese: "去", meaning: "to go"},
+  {pronunciation: "tā", chinese: "她", meaning: "she"},
+  {pronunciation: "hěn", chinese: "很", meaning: "very"},
+  {pronunciation: "kàn", chinese: "看", meaning: "to see; look at"},
+  {pronunciation: "zhīdào", chinese: "知道", meaning: "to know"},
+  {pronunciation: "duì", chinese: "对", meaning: "correct; opposite"},
+  {pronunciation: "dōu", chinese: "都", meaning: "all; both"},
+  {pronunciation: "zǐ", chinese: "子", meaning: "child"},
+  {pronunciation: "yě", chinese: "也", meaning: "also; too"},
+  {pronunciation: "hé", chinese: "和", meaning: "and"},
+  {pronunciation: "xià", chinese: "下", meaning: "below; down"},
+  {pronunciation: "qǐ", chinese: "起", meaning: "to rise"},
+  {pronunciation: "xiè", chinese: "谢", meaning: "to thank"}
 
-  
 
 ]
 
-User.create(name: "Joe Lorenzo", email: "josephclorenzo@gmail.com", years_experience: 3, password: "Stuff")
 Character.create(characters)
-Like.create(character: Character.first, user: User.first)
 
